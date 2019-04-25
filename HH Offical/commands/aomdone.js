50f2fdd88d599543a0e020f89f19381f62d9d99e
MelodicAlbuild <43552157+MelodicAlbuild@users.noreply.github.com>
2019-04-24 20:40:42 -0400

    Added some Vue.js system files
    Really simple Vue.js and database integrations.

---
+++ HH Offical/commands/aomdone.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+let bPing = ("230280611519004672")
+message.channel.send(`<@230280611519004672> AoM Server is down and not returning!`)
+}
+
+module.exports.help = {
+  name: "aomdown"
+}
